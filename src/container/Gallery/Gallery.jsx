2ca7bd7b25cc56157924fce62ab3a5151c26a8e7
Michael <mosayame2002@gmail.com>
2023-10-19 21:02:00 +0100

    update

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import { SubHeading,} from '../../components';
+import { images, data } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => (
-  <div>
-    Gallery
+  <div className='app__gallery flex__center'>
+   <div className="app__gallery-content">
+    <SubHeading title='Instagram' />
+    <h1 className='headtext__cormorant'>Photo Gallery</h1>
+    <p className='p__opensans' style={{color: '#aaa', marginTop: '2rem', marginBottom: '2rem'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+    <button type='button' className='custom__button'>View More</button>
+   </div>
+
+   <div className='app__gallery-image'>
+    <div className="app__gallery-images_container">
+    <img src={images.gallery04} alt="" />
+      <img src={images.gallery02} alt="" />
+      <img src={images.gallery03} alt="" />
+    </div>
+   </div>
   </div>
 );
 
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+// import {BsFillplayFill, BsPauseFill} from 'react-icon/bs'
+import {meal} from '../../constants'
 import './Intro.css';
 
 const Intro = () => (
-  <div>
-    Intro
+  <div className='app__video'>
+    <video src={meal} type='video/mp4' controls='' muted autoPlay={'autoPlay'} preload='auto' loop></video>
+    <div className='app__video-overlay flex-center'>
+      <p className='app__video-overlay-p headtext__cormorant'>Fine Dining</p>
+    </div>
   </div>
 );
 
